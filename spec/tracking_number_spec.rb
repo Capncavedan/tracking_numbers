fde7f2f27320350ff76cd6c0ec24a8ef7fc88ec3
Dan Buettner <dan@danbuettner.net>
2016-05-18 16:29:35 -0500

    modernize spec constructs

--- spec/tracking_number_spec.rb
+++ spec/tracking_number_spec.rb
@@ -6,31 +6,31 @@ describe TrackingNumber do
     describe ".extract_identifier_from(text)" do
       it "finds a (FedEx freight) number in a URL" do
         txt = "https://www.fedex.com/insight/findit/nrp.jsp?tracknumbers=076884980006374&opco=FDEG&language=en&clienttype=ivother"
-        ret = TrackingNumber.extract_identifier_from(txt)
+        ret = TrackingNumber.extract_identifier_from txt
         expect(ret).to include('076884980006374')
       end
 
       it "finds a UPS tracking number within a block of text" do
         txt = "this number 1z0T3731P292258842 is my number"
-        ret = TrackingNumber.extract_identifier_from(txt)
+        ret = TrackingNumber.extract_identifier_from txt
         expect(ret).to include('1Z0T3731P292258842')
       end
 
       it "finds multiple UPS tracking numbers within a block of text" do
         txt = "this number, 1Z0T3731P292258842, and this number, 1Z5FX0076803466397, are my numbers"
-        ret = TrackingNumber.extract_identifier_from(txt)
+        ret = TrackingNumber.extract_identifier_from txt
         expect(ret).to match_array %w(1Z0T3731P292258842 1Z5FX0076803466397)
       end
 
       it "finds multiple UPS tracking numbers within a block of text, and filter duplicates" do
         txt = "this number, 1Z0T3731P292258842, and this number, 1Z5FX0076803466397, are my numbers.  And did I mention 1z5FX0076803466397?"
-        ret = TrackingNumber.extract_identifier_from(txt)
+        ret = TrackingNumber.extract_identifier_from txt
         expect(ret).to match_array %w(1Z0T3731P292258842 1Z5FX0076803466397)
       end
 
       it "finds multiple tracking numbers for various carriers within a block of text" do
         txt = "this number, 1Z0T3731P292258842, and this number, C10999911320231, are my numbers.  And did I mention 9102901000462189604217?"
-        ret = TrackingNumber.extract_identifier_from(txt)
+        ret = TrackingNumber.extract_identifier_from txt
         expect(ret).to match_array %w(1Z0T3731P292258842  9102901000462189604217  C10999911320231)
       end
     end
@@ -96,28 +96,33 @@ describe TrackingNumber do
   context "ONTRAC checksum functions" do
     describe "#ontrac_mapping" do
       it "maps an alpha character A to a numeric character 2" do
-        expect(TrackingNumber.new('').ontrac_mapping('A')).to eq '2'
+        t = TrackingNumber.new ''
+        expect(t.ontrac_mapping 'A').to eq '2'
       end
 
       it "maps an alpha character M to a numeric character 4" do
-        expect(TrackingNumber.new('').ontrac_mapping('M')).to eq '4'
+        t = TrackingNumber.new ''
+        expect(t.ontrac_mapping 'M').to eq '4'
       end
 
       it "does not change a numeric character" do
-        expect(TrackingNumber.new('').ontrac_mapping('3')).to eq '3'
+        t = TrackingNumber.new ''
+        expect(t.ontrac_mapping '3').to eq '3'
       end
     end
 
     describe "#ontrac_checksum_digit" do
       it "returns the last digit from a string, as an integer" do
-        expect(TrackingNumber.new('1234').ontrac_checksum_digit).to eq 4
+        t = TrackingNumber.new '1234'
+        expect(t.ontrac_checksum_digit).to eq 4
       end
     end
 
     describe "#ontrac_core_portion_checksum" do
       it "returns the value derived from the sum of odd-position characters and double the sum of even-position characters" do
         # remember this will be a 1-based array examination, i.e. first character is an odd position
-        expect(TrackingNumber.new('C10101010101010').ontrac_core_portion_checksum).to eq 18
+        t = TrackingNumber.new 'C10101010101010'
+        expect(t.ontrac_core_portion_checksum).to eq 18
       end
     end
 
@@ -162,12 +167,14 @@ describe TrackingNumber do
     describe "#ontrac?" do
       %w(C10999911320231  C10999606576777  C11001105367744  C11000411158855).each do |number|
         it "returns true for #{number}" do
-          expect(TrackingNumber.new(number).ontrac?).to eq true
+          t = TrackingNumber.new number
+          expect(t).to be_ontrac
         end
       end
 
       it "returns false" do
-        expect(TrackingNumber.new('C22222222222222').ontrac?).to eq false
+        t = TrackingNumber.new 'C22222222222222'
+        expect(t).not_to be_ontrac
       end
     end
   end  # of ONTRAC
@@ -176,27 +183,32 @@ describe TrackingNumber do
   context "UPS checksum functions" do
     describe "#ups_mapping" do
       it "maps an alpha character A to a numeric character 2" do
-        expect(TrackingNumber.new('').ups_mapping('A')).to eq '2'
+        t = TrackingNumber.new ''
+        expect(t.ups_mapping 'A').to eq '2'
       end
 
       it "maps an alpha character M to a numeric character 4" do
-        expect(TrackingNumber.new('').ups_mapping('M')).to eq '4'
+        t = TrackingNumber.new ''
+        expect(t.ups_mapping 'M').to eq '4'
       end
 
       it "does not change a numeric character" do
-        expect(TrackingNumber.new('').ups_mapping('3')).to eq '3'
+        t = TrackingNumber.new ''
+        expect(t.ups_mapping '3').to eq '3'
       end
     end
 
     describe "#ups_checksum_digit" do
       it "returns the last digit from a string, as an integer" do
-        expect(TrackingNumber.new('1234').ups_checksum_digit).to eq 4
+        t = TrackingNumber.new '1234'
+        expect(t.ups_checksum_digit).to eq 4
       end
     end
 
     describe "#ups_core_portion" do
       it "return the section between the 1Z and the check digit" do
-        expect(TrackingNumber.new('1Z0101010101010109').ups_core_portion).to eq '010101010101010'
+        t = TrackingNumber.new '1Z0101010101010109'
+        expect(t.ups_core_portion).to eq '010101010101010'
       end
     end
 
@@ -253,13 +265,15 @@ describe TrackingNumber do
     describe "#ups?" do
       %w( 1Z0T3731P292258842  1Z5FX0076803466397  1ZW0X5110319778880 ).each do |number|
         it "returns true for #{number}" do
-          expect(TrackingNumber.new(number).ups?).to eq true
+          t = TrackingNumber.new number
+          expect(t).to be_ups
         end
       end
 
       %w( 9102901000462189604217  bad  1Z0T3731P2922588  1Zinvalid  1Z9999999992804405  1Z  1ZW0X5110319778880678890678342 ).each do |bad_number|
         it "returns false for #{bad_number}" do
-          expect(TrackingNumber.new(bad_number).ups?).to eq false
+          t = TrackingNumber.new bad_number
+          expect(t).not_to be_ups
         end
       end
     end
@@ -267,22 +281,18 @@ describe TrackingNumber do
 
 
   describe "#airborne?" do
-    # it "returns true for a valid number", pending: true do
-    #   expect(TrackingNumber.new('valid airborne').airborne?).to eq true
-    # end
     it "returns false for an invalid number" do
-      expect(TrackingNumber.new('airborne').airborne?).to eq false
+      t = TrackingNumber.new 'airborne'
+      expect(t).not_to be_airborne
     end
   end  # of Airborne
 
 
 
   describe "#dhl?" do
-    # it "returns true for a valid number", pending: true do
-    #   expect(TrackingNumber.new('valid dhl').dhl?).to eq true
-    # end
     it "returns false for an invalid number" do
-      expect(TrackingNumber.new('dhl').dhl?).to eq false
+      t = TrackingNumber.new 'dhl'
+      expect(t).not_to be_dhl
     end
   end  # of DHL
 
@@ -290,58 +300,67 @@ describe TrackingNumber do
   describe "#usps?" do
     %w( EI457881382US  9102901000462189604217 ).each do |number|
       it "returns true for #{number}" do
-        expect(TrackingNumber.new(number).usps?).to eq true
+        t = TrackingNumber.new number
+        expect(t).to be_usps
       end
     end
 
     it "returns false for a bad number" do
-      expect(TrackingNumber.new('8675309').usps?).to eq false
+      t = TrackingNumber.new '8675309'
+      expect(t).not_to be_usps
     end
   end
 
 
   describe "#fedex?" do
-    it "returns true" do
-      expect(TrackingNumber.new('9102901000462189604217').fedex?).to eq true
+    it "returns true for a valid fedex number" do
+      t = TrackingNumber.new '9102901000462189604217'
+      expect(t).to be_fedex
     end
 
-    it "returns false" do
-      expect(TrackingNumber.new('8675309').fedex?).to eq false
+    it "returns false for not-a-fedex-number" do
+      t = TrackingNumber.new '8675309'
+      expect(t).not_to be_fedex
     end
   end
 
 
   describe "UPS tracking numbers" do
     it "returns UPS as a carrier when given a UPS tracking number" do
-      expect(TrackingNumber.new('1Z0T3731P292258842').carriers).to include(:ups)
+      t = TrackingNumber.new '1Z0T3731P292258842'
+      expect(t.carriers).to include(:ups)
     end
   end
 
 
   describe 'USPS tracking numbers' do
     it "returns USPS as a carrier when given a USPS tracking number" do
-      expect(TrackingNumber.new('9102901000462189604217').carriers).to include(:usps)
+      t = TrackingNumber.new '9102901000462189604217'
+      expect(t.carriers).to include(:usps)
     end
   end
 
 
   describe "FedEx" do
     it "returns FedEx as a carrier when given a FedEx tracking number" do
-      expect(TrackingNumber.new('9102927010180027375941').carriers).to include(:fedex)
+      t = TrackingNumber.new '9102927010180027375941'
+      expect(t.carriers).to include(:fedex)
     end
   end
 
 
   describe "FedEx" do
     it "returns FedEx as a carrier when given a FedEx freight tracking number" do
-      expect(TrackingNumber.new('076884980006374').carriers).to include(:fedex)
+      t = TrackingNumber.new '076884980006374'
+      expect(t.carriers).to include(:fedex)
     end
   end
 
 
   describe "FedEx Smartpost" do
     it "returns both FedEx and USPS when given a smartpost number" do
-      expect(TrackingNumber.new('9102927010180027375941').carriers).to match_array [:fedex, :usps]
+      t = TrackingNumber.new '9102927010180027375941'
+      expect(t.carriers).to match_array [:fedex, :usps]
     end
   end
 
